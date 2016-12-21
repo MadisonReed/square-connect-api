a9b21a6f160fdbe72cf89bb133d3e550b16f5d30
cmswalker <c.michael.s.walker@gmail.com>
2016-12-21 09:50:03 -0800

    added testing suite

---
+++ test/index.js
@@ -0,0 +1,26 @@
+const assert = require('assert');
+const path = require('path');
+const sinon = require('sinon');
+
+const imageDir = path.join(__dirname, 'assets', 'images');
+
+const SquareConnect = require(path.join(__dirname, '..', 'lib', 'squareConnect'));
+
+describe('SquareConnect', () => {
+
+  before(done => {
+    done();
+  });
+
+  after(done => {
+    done();
+  });
+
+  describe('v1', () => {
+  });
+
+  describe('v2', () => {
+
+  });
+
+});
